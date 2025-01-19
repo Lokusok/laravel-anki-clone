41458cc46711663f3ba1bcc97a4e1f32be0de8bc
Lokusok <lokusokdrobk@gmail.com>
2025-01-19 13:15:15 +0300

    Add breeze

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-Route::prefix('v1')->group(base_path('routes/v1/api.php'));
+});
